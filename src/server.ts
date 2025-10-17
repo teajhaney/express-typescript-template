d56568bb7ca99c4c4c85606bc2e99ed385613ae3
Yusuf <teajhaney@gmail.com>
2025-10-17 18:07:53 +0100

    Enhance server configuration and middleware functionality
    - Updated nodemon.json to use ES module support with ts-node.
    - Modified package.json to streamline the development script for nodemon.
    - Improved tsconfig.json with stricter type checking and added options for emitting declarations.
    - Refactored src/server.ts to include rate limiting and a not found handler middleware.
    - Introduced async handler utility for better error handling in asynchronous routes.
    - Added not found handler middleware to manage 404 responses.
    - Created a new async.handler.ts file for handling asynchronous route functions.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,41 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
+import express, { Application } from 'express';
+import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import cors from 'cors';
-import errorHandler from '@/middleware/error.hnadler';
 import { PORT } from '@/common/config';
+import errorHandler from '@/middleware/error.hnadler';
+import { notFoundHandler } from '@/middleware/not.found.handler';
+import logger from '@/common/utils/logger';
 
-const app = express();
+const app: Application = express();
 
+//middleware
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//rate limitng
+const endpointRateLimit = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: 'Too many requests',
+    });
+  },
+});
+
+app.use(endpointRateLimit);
+
+//Error handler
+app.use(notFoundHandler);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
